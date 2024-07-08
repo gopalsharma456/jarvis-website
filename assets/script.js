c97a78db520e2fe7784326fbd1c472313bc8a76f
gopal <gopalsharma456@gmail.com>
2024-07-08 15:11:16 +0530

    jarvis website serve section complete

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function productBtn() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementsByClassName("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
