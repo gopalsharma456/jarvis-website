d078fde0e055ce3b81f3724522d033f1ca1de76c
gopal <gopalsharma456@gmail.com>
2024-07-18 15:19:55 +0530

    responsive code add

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
+let x = document.getElementById("product-content");
+let y = document.getElementById("solution-content");
+
 function productBtn() {
-  document.getElementsByClassName("myDropdown").classList.toggle("show");
+  x.classList.toggle("dropdown-content");
+}
+
+function solutionsBtn() {
+  y.classList.toggle("solutions-dropdown-content");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+
+// }
