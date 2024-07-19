f544d19723c5e545cb7498eb4d12559b05d9a625
gopal <gopalsharma456@gmail.com>
2024-07-19 21:06:04 +0530

    partner section code added & project complete

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,37 @@
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 let x = document.getElementById("product-content");
 let y = document.getElementById("solution-content");
 
-function productBtn() {
+function productBtn(event) {
   x.classList.toggle("show");
-  
+  event.stopPropagation();
 }
 
-function solutionsBtn() {
+function solutionsBtn(event) {
   y.classList.toggle("show");
+  event.stopPropagation();
 }
 
 // Close the dropdown menu if the user clicks outside of it
 
 window.onclick = function (event) {
   
-  
 };
 
-function myFunction(){
-  let menuItems = document.getElementById('navbar-items')
-  menuItems.classList.toggle('show')
+function myFunction() {
+  let menuItems = document.getElementById("navbar-items");
+  menuItems.classList.toggle("show");
 }
+
+var swiper = new Swiper(".swiper", {
+  spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
